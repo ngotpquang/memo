a60124010ccc4aecaf3531ca5f3af5096d34364a
ngotpquang <ngotruongphamquang@gmail.com>
2017-11-07 17:12:13 +0700

    Change edit and save memo

--- src/components/memo/Memo.js
+++ src/components/memo/Memo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Memo.css';
 import $ from 'jquery';
+import TextareaAutosize from 'react-autosize-textarea';
 
 class Memo extends Component {
   constructor(props){
@@ -9,6 +10,7 @@ class Memo extends Component {
       colors: ['#c0392b', '#16a085', '#2c3e50'],
     }
   }
+  // onClick={this._editMemo.bind(this)}
     render() {
       let idModal = "modal" + this.props.id;
       let idModalContent = "modal-content" + this.props.id;
@@ -24,18 +26,18 @@ class Memo extends Component {
             <div className="modal-content" id={idModalContent} 
               onMouseOver={this._displayTooltip.bind(this)}
               onMouseLeave={this._hideTooltip.bind(this)}
-              onClick={this._editMemo.bind(this)}>
+              onDoubleClick={this._editMemo.bind(this)}
+              >
               <div className="modal-header">
                 <span className="close" onClick={this._closeModal.bind(this)}>&times;</span>
-                <div className="memo-title">{this.props.title}</div>
+                <input type="text" className="memo-title" defaultValue={this.props.title} disabled/>
               </div>
               <div className="modal-body">
-                <div className="memo-content">
-                  {this.props.content}
-                </div>
+                <TextareaAutosize className="memo-content" rows={10} maxRows={15}  defaultValue={this.props.content} disabled>
+                </TextareaAutosize>
                 <div className="memo-created-date">{this.props.createdDate}</div>
               </div>
-              <div className="tooltip" id={idToolTip}>Click to edit</div>
+              <div className="tooltip" id={idToolTip}>Double click to edit</div>
               <button className="save-button" onClick={this._updateMemo.bind(this)}>Save</button>
               <div className="clear"></div>
             </div>
@@ -48,23 +50,24 @@ class Memo extends Component {
       let randomColor = this.state.colors[this.props.id % 3];
       let memoElement = $('#' + this.props.id);
       let modal = $('#modal-content' +  this.props.id);
+      // let modalHeader = $('#modal-content' +  this.props.id + ' .modal-header');
+      let memoTitleInput = $('#modal-content' +  this.props.id + ' .memo-title');
+      let memoContentTextArea = $('#modal-content' +  this.props.id + ' .memo-content');
+      // let modalBody = $('#modal-content' +  this.props.id + ' .modal-body');
       memoElement.css('background-color', randomColor);
+      memoTitleInput.css('background-color', randomColor);
       modal.css('background-color', randomColor);
+      let color1 = "rgba(255, 255, 255, 0.1)";
+      memoContentTextArea.css('background-color', color1);
     }
 
     _displayModal(){
       let saveButton = $('#modal-content' +  this.props.id + ' .save-button');
       saveButton.hide();      
-      let input = $('#title-input-' + this.props.id);
-      let textareaContent = $('#content-input-' + this.props.id);
-      if (input.html() !== undefined){
-        let memoTitle = $('#modal-content' +  this.props.id + ' .memo-title');
-        let memoContent = $('#modal-content' +  this.props.id + ' .memo-content');
-        memoTitle.show();
-        memoContent.show();
-        input.hide();
-        textareaContent.hide();
-      }
+      let memoTitle = $('#modal-content' +  this.props.id + ' .memo-title');
+      let memoContent = $('#modal-content' +  this.props.id + ' .memo-content');
+      memoTitle.attr("disabled", true);
+      memoContent.attr("disabled", true);
       let modal = $('#modal' +  this.props.id);
       modal.slideDown(500);
     }
@@ -84,41 +87,22 @@ class Memo extends Component {
       let isLoggedIn = localStorage.getItem('userId') !== null;
       let saveButton = $('#modal-content' +  this.props.id + ' .save-button');
       if (isLoggedIn){
-        let input = $('#title-input-' + this.props.id);
         let memoTitle = $('#modal-content' +  this.props.id + ' .memo-title');
         let memoContent = $('#modal-content' +  this.props.id + ' .memo-content');
+        memoTitle.removeAttr("disabled");
+        memoTitle.focus();
+        memoContent.removeAttr("disabled");
         let tooltip = $('#tooltip' +  this.props.id);
         tooltip.slideUp(500);
-        let randomColor = this.state.colors[this.props.id % 3];
-        let modalHeader = $('#modal-content' +  this.props.id + ' .modal-header');
-        if (input.html() === undefined){
-          memoTitle.hide();
-          memoContent.hide();
-          let content = memoContent.html();
-          let title = memoTitle.html();
-          let inputTitle = '<input id="title-input-' + this.props.id + '" class="title" type="text" value="' + title + '" style="background-color:' + randomColor + '" />';
-          let textareaContent = '<textarea id="content-input-' + this.props.id + '" class="content" style="background-color:' + randomColor + '">' + content + '</textarea>';
-          modalHeader.append(inputTitle);
-          modalHeader.append(textareaContent);
-          saveButton.slideDown(500);
-          saveButton.css('color', randomColor);
-        } else {
-          memoTitle.hide();
-          memoContent.hide();
-          let textareaContent = $('#content-input-' + this.props.id);
-          input.show(500);
-          textareaContent.show();
-          saveButton.slideDown();
-        }
-        input.focus();
+        saveButton.slideDown(500);
       }
     }
 
     _displayTooltip(){
       let isLoggedIn = localStorage.getItem('userId') !== null;
       if (isLoggedIn){
-        let input = $('#title-input-' + this.props.id);
-        if (input.html() === undefined || input.css('display') === 'none'){
+        let memoTitle = $('#modal-content' +  this.props.id + ' .memo-title').attr("disabled");
+        if (memoTitle !== undefined){
           let modal = $('#tooltip' +  this.props.id);
           modal.slideDown(500);
         }
@@ -128,8 +112,8 @@ class Memo extends Component {
     _hideTooltip(){
       let isLoggedIn = localStorage.getItem('userId') !== null;
       if (isLoggedIn){
-        let input = $('#title-input-' + this.props.id);
-        if (input.html() === undefined || input.css('display') === 'none'){
+        let memoTitle = $('#modal-content' +  this.props.id + ' .memo-title').attr("disabled");
+        if (memoTitle !== undefined){
           let modal = $('#tooltip' +  this.props.id);
           modal.slideUp(500);
         }
@@ -137,12 +121,11 @@ class Memo extends Component {
     }
 
     _updateMemo(){
-      let title = $('#title-input-' + this.props.id).val();
-      let content = $('#content-input-' + this.props.id).val();
+      let title = $('#modal-content' +  this.props.id + ' .memo-title').val();
+      let content = $('#modal-content' +  this.props.id + ' .memo-content').val();
       let id = this.props.id;
       let userId = localStorage.getItem("userId");
       let data = { title, content, id, userId };
-      console.log(data);
       $.ajax({
         method: 'PUT',
         url: 'http://localhost:8080/api/memo/edit',
@@ -151,16 +134,14 @@ class Memo extends Component {
           'Content-Type': 'application/json'
         },
         success: (result) => {
-          // console.log(result);
           this._closeModal();
-          // window.location.reload();
           this.props.isUpdated();
         },
         error: (error) => {
           // TODO: Display error msg
           // console.log(error);
         }
-      })
+      });
     }
   }
     
